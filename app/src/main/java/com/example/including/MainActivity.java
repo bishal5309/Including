5b3339a64ac60e6ead7b075b2ea1345e6bce69dc
bishal5309 <bishalmd487@gmail.com>
2025-02-27 17:21:00 +0600

    Initial commit

--- app/src/main/java/com/example/including/MainActivity.java
+++ app/src/main/java/com/example/including/MainActivity.java
@@ -4,7 +4,10 @@ import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     AppCompatButton addbtn,removebtn;
 
     LayoutInflater layoutInflater;
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,27 +37,51 @@ public class MainActivity extends AppCompatActivity {
 
 
         main = findViewById(R.id.main);
-        addbtn = findViewById(R.id.add);
-        removebtn = findViewById(R.id.remove);
-       layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-
-
-        addbtn.setOnClickListener(view -> {
-            View myView =  layoutInflater.inflate(R.layout.new_,main);
-        });
-
-
-        removebtn.setOnClickListener(view -> {
-
-        });
+        layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
 
 
 
 
 
+        list = findViewById(R.id.list);
+        MyAdapter myAdapter = new MyAdapter();
+        list.setAdapter(myAdapter);
 
 
+
+
+    }
+
+
+
+    private  class MyAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 10000;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            LayoutInflater layoutInflater1;
+            layoutInflater1 = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View Lview = layoutInflater1.inflate(R.layout.new_,list,false);
+
+
+
+
+            return Lview;
+        }
     }
 }
